98c04fd25cd2196727c01aca60fd7c1ddd5d0d7e
KaRaBe11a <reznikov_den@inbox.ru>
2021-03-31 21:59:41 +0300

    Добавлены функции с N1 по N10

---
+++ Проектище/N1_10.py
@@ -0,0 +1,65 @@
+def COM_NN_D (x, y):    # Определение: какое число больше из двух, или они равны
+    if (x > y):
+        return 2
+    if (x == y):
+        return 0
+    return 1
+
+
+def NZER_N_B (x):      # Определение: равно лли число 0 или нет
+    if (x != 0):
+        return 1
+    return 0
+
+def ADD_1N_N (x):    # Добвление к числу 1
+    x = x + 1
+    return x
+
+def ADD_NN_N (x, y):   # Сумма двух чисел
+    return (x + y)
+
+
+def SUB_NN_N (x, y):            # Вычитание из большего числа меньшее
+    if (COM_NN_D(x, y) == 2):
+        return (x - y)
+    if (COM_NN_D(x, y) == 0):
+        return 0
+    if (COM_NN_D(x, y) == 1):
+        return (y - x)
+
+
+def MUL_ND_N (x, y):  # Умножение двух чисел
+    return (x*y)
+
+def MUL_Nk_N (x, k):   # Умножение числ на 10^k
+    return (x*10^k)
+
+def MUL_NN_N (x, y):    # Умножение натуральных чисел
+    x = ADD_NN_N (x, y)
+    y = MUL_ND_N (x, y)
+    z = MUL_Nk_N (x, y)
+    return (x*y*z)
+
+def SUB_NDN_N (x, y, z):
+    z = MUL_ND_N(z, y)    # Произведение z на y
+    if (z > 0):      # Если z получилось положительным, то возврат вычитания из большего числа меньшее
+        return SUB_NN_N (x, y)
+    return None   # Если z отрицательное, то возврат None
+
+
+
+def DIV_NN_Dk (x, y):
+    k = 0
+    if (COM_NN_D (x, y) == 2):  #  Если x больше y, тогда z присваивается деление x напроизведение y на 10^k
+        z = x / (y * MUL_Nk_N (x, k))
+    if (COM_NN_D (x, y) == 1):    #  Если y больше x, тогда z присваивается деление y напроизведение x на 10^k
+        z = y / (x * MUL_Nk_N(x, k))
+    if (COM_NN_D(x, y) == 0):   # Если x равен y, тогда возврат 1
+        return 1
+    return int(str(abs(z))[0])  # Возврат первой цифры числа - результата верхних вычислений
+
+
+
+
+
+

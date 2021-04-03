664a0f727e4531496b06279ec35ee00b997db291
KaRaBe11a <reznikov_den@inbox.ru>
2021-04-03 18:52:47 +0300

    Адаптировал P6 для работы с рациональными числами

--- Проектище/P6.py
+++ Проектище/P6.py
@@ -1,12 +1,12 @@
 from raspil import rasp
-
+from SRAVN_Q import  SRAVN_Q
 
 def DEG_P_N(mnog):
     razrez = list(rasp(mnog))  # Распиливаем многочлен на отдельные члены
-    maxStep = 0
+    maxStep = "0"
     bufStr = str()
     for i in range(0, len(razrez)):  # Идём по всем членам
-
+        bufStr = ""
         if (razrez[i].count("x") == 1):  # Если в члене есть x
             j = len(razrez[i]) - 1
 
@@ -15,13 +15,13 @@ def DEG_P_N(mnog):
                     bufStr = bufStr + razrez[i][j]
                     j = j - 1
                 bufStr = bufStr[::-1]  # Переворачиваем
-                step = int(bufStr)  # Кидаем в инт
+                step = bufStr  # Кидаем в инт
                 bufStr = ""
 
-                if (step > maxStep):  # Проверяем с максимальной степенью
+                if (SRAVN_Q(step, maxStep) == 2):  # Проверяем с максимальной степенью
                     maxStep = step
             else:  # Если x в 1 степени
-                if (maxStep < 1):
-                    maxStep = 1
+                if (SRAVN_Q("1", maxStep) == 2):
+                    maxStep = "1"
     return maxStep  # Если x ов нигде не было вернёт 0
 
